58cd5ebd6183da94ea10c0b84ee64b2f68ec6d8a
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-01-12 16:24:49 +0000

    Added help and preferences scenes

--- gpsDashboard/app/assistants/main-assistant.js
+++ gpsDashboard/app/assistants/main-assistant.js
@@ -1,3 +1,4 @@
+
 function MainAssistant() {
 }
 
@@ -15,4 +16,26 @@ MainAssistant.prototype.deactivate = function(event) {
 }
 
 MainAssistant.prototype.cleanup = function(event) {
+}
+
+MainAssistant.prototype.handleCommand = function (event) {
+	if (event.type == Mojo.Event.commandEnable &&
+	    (event.command == Mojo.Menu.helpCmd)) {
+         event.stopPropagation(); 
+    }
+	if (event.type == Mojo.Event.commandEnable &&
+	    (event.command == Mojo.Menu.prefsCmd)) {
+         event.stopPropagation(); 
+    }
+
+	if (event.type == Mojo.Event.command) {
+		switch (event.command) {
+			case Mojo.Menu.helpCmd:
+				Mojo.Controller.stageController.pushScene('support');
+				break;
+			case Mojo.Menu.prefsCmd:
+				Mojo.Controller.stageController.pushScene('preferences');
+				break;
+		}
+	}	
 }
---
+++ gpsDashboard/app/assistants/support-assistant.js
@@ -0,0 +1,15 @@
+function SupportAssistant() {
+}
+
+SupportAssistant.prototype.setup = function() {
+}
+
+SupportAssistant.prototype.activate = function(event) {
+}
+
+
+SupportAssistant.prototype.deactivate = function(event) {
+}
+
+SupportAssistant.prototype.cleanup = function(event) {
+}
