0617b5dcd808fd7020ef4c5562bf5ed935d2ccc9
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-01-12 03:51:29 +0000

    Basic structure

--- gpsDashboard/app/assistants/main-assistant.js
+++ gpsDashboard/app/assistants/main-assistant.js
@@ -2,6 +2,9 @@ function MainAssistant() {
 }
 
 MainAssistant.prototype.setup = function() {
+	if (this.controller.stageController.setWindowOrientation) {
+		this.controller.stageController.setWindowOrientation("free");
+	}	
 }
 
 MainAssistant.prototype.activate = function(event) {
