f8a01fd61f8efc68eb7ed9ad5058cc0b7fea1388
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-01-14 22:29:15 +0000

    Updated gps perm error message to be more helpful Added some infrastructure for calculating speed & distance based on location Reduced the font size to allow more room to grow.

--- gpsDashboard/app/assistants/main-assistant.js
+++ gpsDashboard/app/assistants/main-assistant.js
@@ -4,6 +4,9 @@
 gpsDashboard = { };
 gpsDashboard.units = 1;
 gpsDashboard.backlight = 1;
+gpsDashboard.avgSpeed = { count: 0, value: 0 };
+gpsDashboard.prevLoc = { };
+gpsDashboard.tripometer = 0;
 
 gpsDashboard.cookie = ({
 	initialize: function() {
@@ -61,71 +64,49 @@ MainAssistant.prototype.activate = function(event) {
 	);
 }
 
-MainAssistant.prototype.handleServiceResponse = function(event) {
+MainAssistant.prototype.handleServiceResponse = function( event ) {
 	this.controller.get('announce').update("");	
 	this.scrim.hide();
 	if (event.errorCode == 0) 
-	{	if ( (event.heading >= 348.75 && event.heading <= 360) ||
-			 (event.heading >=0 && event.heading < 11.25) )
-			this.controller.get('heading').update("Heading: North");
-		if (event.heading >= 11.25 && event.heading < 33.75)
-			this.controller.get('heading').update("Heading: North-northeast");
-		if (event.heading >= 33.75 && event.heading < 56.25)
-			this.controller.get('heading').update("Heading: Northeast");
-		if (event.heading >= 56.25 && event.heading < 78.75)
-			this.controller.get('heading').update("Heading: East-northeast");
-		if (event.heading >= 78.75 && event.heading < 101.25)
-			this.controller.get('heading').update("Heading: East");
-		if (event.heading >= 101.25 && event.heading < 123.75)
-			this.controller.get('heading').update("Heading: East-southeast");
-		if (event.heading >= 123.75 && event.heading < 146.25)
-			this.controller.get('heading').update("Heading: Southeast");
-		if (event.heading >= 146.25 && event.heading < 168.75)
-			this.controller.get('heading').update("Heading: South-southeast");
-		if (event.heading >= 168.75 && event.heading < 191.25)
-			this.controller.get('heading').update("Heading: South");
-		if (event.heading >= 191.25 && event.heading < 213.75)
-			this.controller.get('heading').update("Heading: South-southwest");
-		if (event.heading >= 213.75 && event.heading < 236.25)
-			this.controller.get('heading').update("Heading: Southwest");
-		if (event.heading >= 236.25 && event.heading < 258.75)
-			this.controller.get('heading').update("Heading: West-southwest");
-		if (event.heading >= 258.75 && event.heading < 281.25)
-			this.controller.get('heading').update("Heading: West");
-		if (event.heading >= 281.25 && event.heading < 303.75)
-			this.controller.get('heading').update("Heading: West-northwest");
-		if (event.heading >= 303.75 && event.heading < 326.25)
-			this.controller.get('heading').update("Heading: Northwest");
-		if (event.heading >= 326.25 && event.heading < 348.75)
-			this.controller.get('heading').update("Heading: North-northwest");
-
-		if (event.velocity == 0) {
-			this.controller.get('speed').update("speed: -");
-			this.controller.get('heading').update("Heading: -");
-		}
-		else if (gpsDashboard.units == 1)
-			this.controller.get('speed').update("Speed: " + (event.velocity * 2.23693629).toFixed(1) + " mph");
-		else if (gpsDashboard.units == 2)
-			this.controller.get('speed').update("Speed: " + (event.velocity * 3.6).toFixed(1) + " kph");
-			
-		if (event.vertAccuracy == 0)
-			this.controller.get('altitude').update("Altitude: -");
-		else if (gpsDashboard.units == 1)
-			this.controller.get('altitude').update("Altitude: " + (event.altitude * 3.2808399).toFixed(1) + " feet");
-		else if (gpsDashboard.units == 2)
-			this.controller.get('altitude').update("Altitude: " + event.altitude.toFixed(1) + " meters");
-
-		if (event.horizAccuracy > 999)
-			this.controller.get('accuracy').update("Accuracy:  Poor");
-		else if (event.horizAccuracy > 99)
-			this.controller.get('accuracy').update("Accuracy:  Medium");
-		else if (event.horizAccuracy > 9) 
-			this.controller.get('accuracy').update("Accuracy:  Good");
-		else
-			this.controller.get('accuracy').update("Accuracy: Excellent");
+	{	
+		this.controller.get('heading').update(this.heading(event));
+		this.controller.get('speed').update(this.speed(event));
+		this.controller.get('altitude').update(this.altitude(event));
+		this.controller.get('accuracy').update(this.accuracy(event));
+		this.controller.get('calcSpeed').update(this.calcSpeed(event));
+		gpsDashboard.prevLoc = event;
 	}
 	else
 		this.controller.stageController.pushScene("gpsError", event.errorCode);
+}
+
+MainAssistant.prototype.calcSpeed = function( event ){
+	if (!gpsDashboard.prevLoc)
+		return "Calculated Speed: -";
+	if (gpsDashboard.units = 1)
+		return "Calculated Speed: " +
+			(this.calcDist(gpsDashboard.prevLoc, event) / 
+			 this.calcTime(gpsDashboard.prevLoc, event) * .621371192).toFixed(1) + " mph";
+	if (gpsDashboard.units = 2)
+		return "Calculated Speed: " +
+			(this.calcDist(gpsDashboard.prevLoc, event) / 
+		 	 this.calcTime(gpsDashboard.prevLoc, event)).toFixed(1) + " kph";
+}
+MainAssistant.prototype.calcTime = function( event1, event2 ) {
+	return (event2.timestamp - event1.timestamp) / 1000;
+}
+MainAssistant.prototype.calcDist = function( point1, point2 ) {
+	dLat = point2.latitude - point1.latitude;
+	dLon = point2.longitude - point1.longitude;
+	mLat = ((point2.latitude + point1.latitude) / 2).toRad();
+	K1 = 111.13209 - 
+		 (0.56605 * Math.cos(2 * mLat)) + 
+		 (0.00120 * Math.cos(4 * mLat));
+	K2 = (111.41513 * Math.cos(mLat)) - 
+		 (0.09455 * Math.cos(3 * mLat)) + 
+		 (0.00012 * Math.cos(5 * mLat));
+	dist = Math.sqrt( Math.pow((K1 * dLat), 2) + Math.pow((K2 * dLon), 2));
+	return dist;
 }
 
 MainAssistant.prototype.handleServiceResponseError = function(event) {
@@ -133,6 +114,7 @@ MainAssistant.prototype.handleServiceResponseError = function(event) {
 }
 
 MainAssistant.prototype.deactivate = function(event) {
+	this.controller.get('speed').update("Deactivated");
 	this.controller.stageController.setWindowProperties({blockScreenTimeout: false});
 	this.trackingHandle.cancel(); 
 }
@@ -163,4 +145,69 @@ MainAssistant.prototype.handleCommand = function (event) {
 				break;
 		}
 	}
+}
+MainAssistant.prototype.heading = function(event){
+	if (event.velocity == 0)
+		return "Heading: -";
+	if ((event.heading >= 348.75 && event.heading <= 360) ||
+		(event.heading >= 0 && event.heading < 11.25)		) 
+		return "Heading: North";
+	if (event.heading >= 11.25 && event.heading < 33.75) 
+		return "Heading: North-northeast";
+	if (event.heading >= 33.75 && event.heading < 56.25) 
+		return "Heading: Northeast";
+	if (event.heading >= 56.25 && event.heading < 78.75) 
+		return "Heading: East-northeast";
+	if (event.heading >= 78.75 && event.heading < 101.25) 
+		return "Heading: East";
+	if (event.heading >= 101.25 && event.heading < 123.75) 
+		return "Heading: East-southeast";
+	if (event.heading >= 123.75 && event.heading < 146.25) 
+		return "Heading: Southeast";
+	if (event.heading >= 146.25 && event.heading < 168.75) 
+		return "Heading: South-southeast";
+	if (event.heading >= 168.75 && event.heading < 191.25) 
+		return "Heading: South";
+	if (event.heading >= 191.25 && event.heading < 213.75) 
+		return "Heading: South-southwest";
+	if (event.heading >= 213.75 && event.heading < 236.25) 
+		return "Heading: Southwest";
+	if (event.heading >= 236.25 && event.heading < 258.75) 
+		return "Heading: West-southwest";
+	if (event.heading >= 258.75 && event.heading < 281.25) 
+		return "Heading: West";
+	if (event.heading >= 281.25 && event.heading < 303.75) 
+		return "Heading: West-northwest";
+	if (event.heading >= 303.75 && event.heading < 326.25) 
+		return "Heading: Northwest";
+	if (event.heading >= 326.25 && event.heading < 348.75) 
+		return "Heading: North-northwest";
+}
+MainAssistant.prototype.speed = function(event) {
+	if (event.velocity == 0)
+		return "Speed: -";
+	if (gpsDashboard.units == 1)
+		return "Speed: " + (event.velocity * 2.23693629).toFixed(1) + " mph";
+	if (gpsDashboard.units == 2)
+		return "Speed: " + (event.velocity * 3.6).toFixed(1) + " kph";
+}
+MainAssistant.prototype.altitude = function(event){
+	if (event.vertAccuracy == 0)
+		return "Altitude: -";
+	if (gpsDashboard.units == 1)
+		return "Altitude: " + (event.altitude * 3.2808399).toFixed(1) + " feet";
+	if (gpsDashboard.units == 2)
+		return "Altitude: " + event.altitude.toFixed(1) + " meters";
+}
+MainAssistant.prototype.accuracy = function(event){
+	if (event.horizAccuracy > 999) 
+		return "Accuracy:  Poor";
+	if (event.horizAccuracy > 99) 
+		return "Accuracy:  Medium";
+	if (event.horizAccuracy > 9) 
+		return "Accuracy:  Good";
+	return "Accuracy: Excellent";
+}
+Number.prototype.toRad = function() {  // convert degrees to radians
+  return this * Math.PI / 180;
 }
