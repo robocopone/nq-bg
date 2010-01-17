d6d8854203a5882f29b8c556acf94f0ac66a2be4
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-01-17 22:20:47 +0000

    Added Avg Speed preference Added logic to only accept excellent fixes for updating trip info

--- gpsDashboard/app/assistants/preferences-assistant.js
+++ gpsDashboard/app/assistants/preferences-assistant.js
@@ -1,53 +1,65 @@
 function PreferencesAssistant() {
 }
 
-PreferencesAssistant.prototype.setup = function() {
+PreferencesAssistant.prototype.setup = function(){
 	//Units Selector Widget
-	this.controller.setupWidget(
-		"unitsSelector",
-		this.unitsSelectorAttributes = {
-			label: "Units",
-			choices: [
-				{label: "Miles/Feet", value: 1},
-				{label: "Kilometers/Meters", value: 2},
-			],
-		},
-		this.unitsSelectorModel = {
-			value: gpsDashboard.units,
-			disabled: false
-		}
-	);
+	this.controller.setupWidget("unitsSelector", this.unitsSelectorAttributes = {
+		label: "Units",
+		choices: [{
+			label: "Miles/Feet",
+			value: 1
+		}, {
+			label: "Kilometers/Meters",
+			value: 2
+		}, ],
+	}, this.unitsSelectorModel = {
+		value: gpsDashboard.units,
+		disabled: false
+	});
 	//Units Selector Handler
 	this.changeUnitsSelectorHandler = this.changeUnitsSelector.bindAsEventListener(this);
-	this.controller.listen(
-		"unitsSelector",
-		Mojo.Event.propertyChange,
-		this.changeUnitsSelectorHandler
-	);
+	this.controller.listen("unitsSelector", Mojo.Event.propertyChange, this.changeUnitsSelectorHandler);
 	
 	//Backlight Selector Widget
-	this.controller.setupWidget(
-		"backlightSelector",
-		this.backlightSelectorAttributes = {
-			label: "Backlight",
-			choices: [
-				{label: "Always On", value: 1},
-				{label: "Device Settings", value: 2},
-			],
-		},
-		this.backlightSelectorModel = {
-			value: gpsDashboard.backlight,
-			disabled: false
-		}
-	);
+	this.controller.setupWidget("backlightSelector", this.backlightSelectorAttributes = {
+		label: "Backlight",
+		choices: [{
+			label: "Always On",
+			value: 1
+		}, {
+			label: "Device Settings",
+			value: 2
+		}, ],
+	}, this.backlightSelectorModel = {
+		value: gpsDashboard.backlight,
+		disabled: false
+	});
 	//Backlight Selector Handler
 	this.changeBacklightSelectorHandler = this.changeBacklightSelector.bindAsEventListener(this);
-	this.controller.listen(
-		"backlightSelector", 
-		Mojo.Event.propertyChange,
-		this.changeBacklightSelectorHandler
-	);
+	this.controller.listen("backlightSelector", Mojo.Event.propertyChange, this.changeBacklightSelectorHandler);
 
+	//Avg Speed Widget
+	this.controller.setupWidget("avgSpeedPref", this.avgSpeedAttributes = {
+		label: "Avg. Speed",
+		choices: [{
+			label: "Use Dist. from Initial",
+			value: 1
+		}, {
+			label: "Use Dist Traveled",
+			value: 2
+		}, ],
+	}, this.avgSpeedModel = {
+		value: gpsDashboard.avgSpeedPref,
+		disabled: false
+	});
+	//Avg Speed Selector Handler
+	this.changeAvgSpeedHandler = this.changeAvgSpeed.bindAsEventListener(this);
+	this.controller.listen("avgSpeed", Mojo.Event.propertyChange, this.changeAvgSpeedHandler);
+	
+}
+PreferencesAssistant.prototype.changeAvgSpeed = function (event) {
+	gpsDashboard.avgSpeedPref = this.avgSpeedModel.value;
+	gpsDashboard.cookie.storeCookie();
 }
 PreferencesAssistant.prototype.changeUnitsSelector = function(event) {
 	gpsDashboard.units = this.unitsSelectorModel.value;
@@ -65,5 +77,7 @@ PreferencesAssistant.prototype.activate = function(event) {
 PreferencesAssistant.prototype.deactivate = function(event) {
 }
 
-PreferencesAssistant.prototype.cleanup = function(event) {
+PreferencesAssistant.prototype.cleanup = function(event){
+	this.controller.stopListening("backlightSelector", Mojo.Event.propertyChange, this.changeBacklightSelectorHandler);
+	this.controller.stopListening("unitsSelector", Mojo.Event.propertyChange, this.changeUnitsSelectorHandler);
 }
