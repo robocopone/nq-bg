5f377b9d0baeb59060021cf9822a0adc6cb614d7
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-01-20 16:13:29 +0000

    Added the records scene, and added a slightly larger margin for Current info

---
+++ trunk/gpsDashboardpaid/app/assistants/records-assistant.js
@@ -0,0 +1,15 @@
+function RecordsAssistant() {
+}
+
+RecordsAssistant.prototype.setup = function() {
+}
+
+RecordsAssistant.prototype.activate = function(event) {
+}
+
+
+RecordsAssistant.prototype.deactivate = function(event) {
+}
+
+RecordsAssistant.prototype.cleanup = function(event) {
+}
