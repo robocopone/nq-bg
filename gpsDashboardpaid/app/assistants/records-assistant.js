828d178ef6b38bf4a9cbccf5178e7e79224a52de
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-01-20 22:03:48 +0000

    Records started

--- gpsDashboardpaid/app/assistants/records-assistant.js
+++ gpsDashboardpaid/app/assistants/records-assistant.js
@@ -2,6 +2,7 @@ function RecordsAssistant() {
 }
 
 RecordsAssistant.prototype.setup = function() {
+	this.controller.get('alltimeHigh').update(gpsDashboard.alltimeHigh.data.altitude);
 }
 
 RecordsAssistant.prototype.activate = function(event) {
