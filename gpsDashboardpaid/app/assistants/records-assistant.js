671b9aaca5e11c76e6d99feadf9e577dbf8a2092
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-01-20 22:48:32 +0000

    Finished rudimentary display of records for testing purposes Fixed the inital display that was broken due to the header spacing

--- gpsDashboardpaid/app/assistants/records-assistant.js
+++ gpsDashboardpaid/app/assistants/records-assistant.js
@@ -2,7 +2,22 @@ function RecordsAssistant() {
 }
 
 RecordsAssistant.prototype.setup = function() {
-	this.controller.get('alltimeHigh').update(gpsDashboard.alltimeHigh.data.altitude);
+	if (gpsDashboard.units == 1)
+		this.controller.get('alltimeTopSpeed').update(
+			(gpsDashboard.alltimeTopSpeed.data.velocity * 2.23693629).toFixed(1) + $L(" mph"));
+	if (gpsDashboard.units == 2)
+		this.controller.get('alltimeTopSpeed').update(
+			(gpsDashboard.alltimeTopSpeed.data.velocity * 3.6).toFixed(1) + $L(" kph"));
+	if (gpsDashboard.units == 1) {
+		this.controller.get('alltimeHigh').update(
+			(gpsDashboard.alltimeHigh.data.altitude * 3.2808399).toFixed(1) + $L(" feet"));
+		this.controller.get('alltimeLow').update(
+			(gpsDashboard.alltimeLow.data.altitude * 3.2808399).toFixed(1) + $L(" feet"));
+	}
+	if (gpsDashboard.units == 2) {
+		this.controller.get('alltimeHigh').update(gpsDashboard.alltimeHigh.data.altitude + $L(" meters"));
+		this.controller.get('alltimeLow').update(gpsDashboard.alltimeLow.data.altitude + $L("meters"));
+	}
 }
 
 RecordsAssistant.prototype.activate = function(event) {
