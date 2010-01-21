d87b8d99a9f04fa0b8fc49118770259666ace58e
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-01-21 22:04:49 +0000

    Added resets to the records scene added shake for resetting things with preference option added color indicator for speed vs average speed with preference option

--- gpsDashboardpaid/app/assistants/records-assistant.js
+++ gpsDashboardpaid/app/assistants/records-assistant.js
@@ -1,32 +1,78 @@
 function RecordsAssistant() {
 }
 
-RecordsAssistant.prototype.setup = function() {
-	if (gpsDashboard.units == 1) 
-		this.controller.get('alltimeTopSpeed').update(
-			(gpsDashboard.alltimeTopSpeed.data.velocity * 2.23693629).toFixed(1) + $L(" mph"));
-	if (gpsDashboard.units == 2)
-		this.controller.get('alltimeTopSpeed').update(
-			(gpsDashboard.alltimeTopSpeed.data.velocity * 3.6).toFixed(1) + $L(" kph"));
+RecordsAssistant.prototype.setup = function(){
 
-	if (gpsDashboard.units == 1) {
-		this.controller.get('alltimeHigh').update(
-			(gpsDashboard.alltimeHigh.data.altitude * 3.2808399).toFixed(1) + $L(" feet"));
-		this.controller.get('alltimeLow').update(
-			(gpsDashboard.alltimeLow.data.altitude * 3.2808399).toFixed(1) + $L(" feet"));
-	}
-	if (gpsDashboard.units == 2) {
+	if (gpsDashboard.units == 1 && gpsDashboard.alltimeTopSpeed.data.velocity != 0)
+		this.controller.get('alltimeTopSpeed').update((gpsDashboard.alltimeTopSpeed.data.velocity * 2.23693629).toFixed(1) + $L(" mph"));
+	if (gpsDashboard.units == 2 && gpsDashboard.alltimeTopSpeed.data.velocity != 0) 
+		this.controller.get('alltimeTopSpeed').update((gpsDashboard.alltimeTopSpeed.data.velocity * 3.6).toFixed(1) + $L(" kph"));
+	
+	if (gpsDashboard.units == 1 && gpsDashboard.alltimeHigh.data.altitude != 0)
+		this.controller.get('alltimeHigh').update((gpsDashboard.alltimeHigh.data.altitude * 3.2808399).toFixed(1) + $L(" feet"));
+	if (gpsDashboard.units == 1 && gpsDashboard.alltimeLow.data.altitude != 15000)
+		this.controller.get('alltimeLow').update((gpsDashboard.alltimeLow.data.altitude * 3.2808399).toFixed(1) + $L(" feet"));
+	if (gpsDashboard.units == 2 && gpsDashboard.alltimeHigh.data.altitude != 0) 
 		this.controller.get('alltimeHigh').update(gpsDashboard.alltimeHigh.data.altitude + $L(" meters"));
+	if (gpsDashboard.units == 2 && gpsDashboard.alltimeHigh.data.altitude != 15000) 
 		this.controller.get('alltimeLow').update(gpsDashboard.alltimeLow.data.altitude + $L(" meters"));
-	}
-
+	
 	this.controller.get('alltimeTopSpeedDate').update(gpsDashboard.alltimeTopSpeed.date);
 	this.controller.get('alltimeHighDate').update(gpsDashboard.alltimeHigh.date);
 	this.controller.get('alltimeLowDate').update(gpsDashboard.alltimeLow.date);
 
-	this.controller.listen(this.controller.get('alltimeTopGroup'),Mojo.Event.tap, this.alltimeTopGroup.bindAsEventListener(this));
-	this.controller.listen(this.controller.get('alltimeHighGroup'),Mojo.Event.tap, this.alltimeHighGroup.bindAsEventListener(this));
-	this.controller.listen(this.controller.get('alltimeLowGroup'),Mojo.Event.tap, this.alltimeLowGroup.bindAsEventListener(this));
+	this.controller.listen(this.controller.get('alltimeTopGroup'), Mojo.Event.tap, this.alltimeTopGroup.bindAsEventListener(this));
+	this.controller.listen(this.controller.get('alltimeHighGroup'), Mojo.Event.tap, this.alltimeHighGroup.bindAsEventListener(this));
+	this.controller.listen(this.controller.get('alltimeLowGroup'), Mojo.Event.tap, this.alltimeLowGroup.bindAsEventListener(this));
+	
+	this.controller.setupWidget('resets', this.atts = {}, this.model = {
+		buttonLabel: 'Resets',
+		buttonClass: 'negative',
+		disabled: false
+	});
+	this.controller.listen(this.controller.get('resets'), Mojo.Event.tap, this.resets.bindAsEventListener(this));
+	
+}
+RecordsAssistant.prototype.resets = function(){
+	this.controller.showAlertDialog({
+		onChoose: this.doResets,
+		title: $L("Resets"),
+		message: $L("Choose one of the following options"),
+		choices: [{
+			label: $L('Top Speed'),
+			value: 'topSpeed',
+			type: 'negative'
+		}, {
+			label: $L('Highest Elevation'),
+			value: 'alltimeHigh',
+			type: 'negative'
+		}, {
+			label: $L('Lowest Elevation'),
+			value: 'alltimeLow',
+			type: 'negative'
+		}, ]
+	});
+}
+RecordsAssistant.prototype.doResets = function (choice) {
+	if (choice == 'topSpeed') {
+		gpsDashboard.alltimeTopSpeed.data.velocity = 0;
+		gpsDashboard.alltimeTopSpeed.data.latitude = undefined;
+		this.controller.get('alltimeTopSpeed').update("");
+		this.controller.get('alltimeTopGroup').update("");
+	}
+	if (choice == 'alltimeHigh') {
+		gpsDashboard.alltimeHigh.data.altitude = 0;
+		gpsDashboard.alltimeHigh.data.latitude = undefined;
+		this.controller.get('alltimeHigh').update("");
+		this.controller.get('alltimeHighDate').update("");
+	}
+	if (choice == 'alltimeLow') {
+		gpsDashboard.alltimeLow.data.altitude = 15000;
+		gpsDashboard.alltimeLow.data.latitude = undefined;
+		this.controller.get('alltimeLow').update("");
+		this.controller.get('alltimeLowDate').update("");
+	}
+	
 }
 
 RecordsAssistant.prototype.alltimeTopGroup = function () {
@@ -79,4 +125,5 @@ RecordsAssistant.prototype.cleanup = function(event) {
 	this.controller.stopListening(this.controller.get('alltimeTopGroup'),Mojo.Event.tap, this.alltimeTopGroup.bindAsEventListener(this));
 	this.controller.stopListening(this.controller.get('alltimeHighGroup'),Mojo.Event.tap, this.alltimeHighGroup.bindAsEventListener(this));
 	this.controller.stopListening(this.controller.get('alltimeLowGroup'),Mojo.Event.tap, this.alltimeLowGroup.bindAsEventListener(this));
+	this.controller.stopListening(this.controller.get('resets'), Mojo.Event.tap, this.resets.bindAsEventListener(this));
 }
