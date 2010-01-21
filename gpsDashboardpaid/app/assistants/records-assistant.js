4e9c499a1342fadc69bb4348552da78496a85bd3
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-01-21 04:18:32 +0000

    Finished up the records section. Added info in the acceptace error Fixed some style issues

--- gpsDashboardpaid/app/assistants/records-assistant.js
+++ gpsDashboardpaid/app/assistants/records-assistant.js
@@ -2,12 +2,13 @@ function RecordsAssistant() {
 }
 
 RecordsAssistant.prototype.setup = function() {
-	if (gpsDashboard.units == 1)
+	if (gpsDashboard.units == 1) 
 		this.controller.get('alltimeTopSpeed').update(
 			(gpsDashboard.alltimeTopSpeed.data.velocity * 2.23693629).toFixed(1) + $L(" mph"));
 	if (gpsDashboard.units == 2)
 		this.controller.get('alltimeTopSpeed').update(
 			(gpsDashboard.alltimeTopSpeed.data.velocity * 3.6).toFixed(1) + $L(" kph"));
+
 	if (gpsDashboard.units == 1) {
 		this.controller.get('alltimeHigh').update(
 			(gpsDashboard.alltimeHigh.data.altitude * 3.2808399).toFixed(1) + $L(" feet"));
@@ -16,8 +17,55 @@ RecordsAssistant.prototype.setup = function() {
 	}
 	if (gpsDashboard.units == 2) {
 		this.controller.get('alltimeHigh').update(gpsDashboard.alltimeHigh.data.altitude + $L(" meters"));
-		this.controller.get('alltimeLow').update(gpsDashboard.alltimeLow.data.altitude + $L("meters"));
+		this.controller.get('alltimeLow').update(gpsDashboard.alltimeLow.data.altitude + $L(" meters"));
 	}
+
+	this.controller.get('alltimeTopSpeedDate').update(gpsDashboard.alltimeTopSpeed.date);
+	this.controller.get('alltimeHighDate').update(gpsDashboard.alltimeHigh.date);
+	this.controller.get('alltimeLowDate').update(gpsDashboard.alltimeLow.date);
+
+	this.controller.listen(this.controller.get('alltimeTopGroup'),Mojo.Event.tap, this.alltimeTopGroup.bindAsEventListener(this));
+	this.controller.listen(this.controller.get('alltimeHighGroup'),Mojo.Event.tap, this.alltimeHighGroup.bindAsEventListener(this));
+	this.controller.listen(this.controller.get('alltimeLowGroup'),Mojo.Event.tap, this.alltimeLowGroup.bindAsEventListener(this));
+}
+
+RecordsAssistant.prototype.alltimeTopGroup = function () {
+	if (gpsDashboard.alltimeTopSpeed.data.latitude) {
+		this.controller.serviceRequest('palm://com.palm.applicationManager', {
+		    method: 'launch',
+		    parameters: {
+		        id:"com.palm.app.maps",
+				params:{"query": gpsDashboard.alltimeTopSpeed.data.latitude + "," +
+					gpsDashboard.alltimeTopSpeed.data.longitude}
+		    }
+		});
+	}	
+}
+
+RecordsAssistant.prototype.alltimeHighGroup = function () {
+	if (gpsDashboard.alltimeHigh.data.latitude) {
+		this.controller.serviceRequest('palm://com.palm.applicationManager', {
+		    method: 'launch',
+		    parameters: {
+		        id:"com.palm.app.maps",
+				params:{"query": gpsDashboard.alltimeHigh.data.latitude + "," +
+					gpsDashboard.alltimeHigh.data.longitude}
+		    }
+		});
+	}	
+}
+
+RecordsAssistant.prototype.alltimeLowGroup = function () {
+	if (gpsDashboard.alltimeLow.data.latitude) {
+		this.controller.serviceRequest('palm://com.palm.applicationManager', {
+		    method: 'launch',
+		    parameters: {
+		        id:"com.palm.app.maps",
+				params:{"query": gpsDashboard.alltimeLow.data.latitude + "," +
+					gpsDashboard.alltimeLow.data.longitude}
+		    }
+		});
+	}	
 }
 
 RecordsAssistant.prototype.activate = function(event) {
@@ -28,4 +76,7 @@ RecordsAssistant.prototype.deactivate = function(event) {
 }
 
 RecordsAssistant.prototype.cleanup = function(event) {
+	this.controller.stopListening(this.controller.get('alltimeTopGroup'),Mojo.Event.tap, this.alltimeTopGroup.bindAsEventListener(this));
+	this.controller.stopListening(this.controller.get('alltimeHighGroup'),Mojo.Event.tap, this.alltimeHighGroup.bindAsEventListener(this));
+	this.controller.stopListening(this.controller.get('alltimeLowGroup'),Mojo.Event.tap, this.alltimeLowGroup.bindAsEventListener(this));
 }
