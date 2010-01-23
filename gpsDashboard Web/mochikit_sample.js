2d1f0da1409eae511c00c4af1c8695bac146eb0d
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-01-23 02:46:34 +0000

    asdf

--- gpsDashboard Web/mochikit_sample.js
+++
@@ -1,9 +0,0 @@
-
-var roundedCornersOnLoad = function () {
-    swapDOM("visual_version", SPAN(null, MochiKit.Visual.VERSION));
-    roundClass("h1", null);
-    roundClass("h2", null, {corners: "bottom"});
-};
-
-addLoadEvent(roundedCornersOnLoad);
-
