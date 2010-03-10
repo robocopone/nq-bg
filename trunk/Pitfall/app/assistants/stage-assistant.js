c1a6fc294be70dc377294ba05bea2446efff7ba6
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-03-10 20:55:43 +0000

    misc bullshit

--- trunk/Pitfall/app/assistants/stage-assistant.js
+++ trunk/Pitfall/app/assistants/stage-assistant.js
@@ -3,7 +3,7 @@ function StageAssistant() {
 
 StageAssistant.prototype.setup = function(){
 	this.controller.setWindowOrientation('left');
-	this.controller.setWindowProperties("fastAccelerometer");	
+	this.controller.setWindowProperties('fastAccelerometer');	
 	this.controller.pushScene({
 		name: "main",
 		transition: Mojo.Transition.crossFade,
