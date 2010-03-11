9f0c8c7a6d33253d1499ffbff4f35f7f61521a67
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-03-11 23:02:48 +0000

    High Scores & game replay finished

--- trunk/Pitfall/app/assistants/scoring-assistant.js
+++ trunk/Pitfall/app/assistants/scoring-assistant.js
@@ -5,7 +5,13 @@ ScoringAssistant.prototype.setup = function() {
 }
 
 ScoringAssistant.prototype.activate = function(event) {
-	this.controller.get('finalScore').update(global.score)
+	for (var x = 1; x <= 10; x++) {
+		if (global.scores[x]) {
+			this.controller.get('num').update(x);
+			this.controller.get('name' + x).update(global.scores[x].name)
+			this.controller.get('score' + x).update(global.scores[x].score)
+		}
+	}
 }
 
 
