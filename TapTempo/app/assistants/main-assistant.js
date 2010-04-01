f7f8fbdc2ab7006ac9b878427fe809c80f031db5
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-04-01 19:16:27 +0000

    v1.1.0

--- TapTempo/app/assistants/main-assistant.js
+++ TapTempo/app/assistants/main-assistant.js
@@ -5,36 +5,100 @@ tapTempo.elements = {}
 tapTempo.time = new Date().getTime()
 tapTempo.count = 0;
 tapTempo.duration = 0;
+tapTempo.prevDuration = 0;
+tapTempo.resetDuration = 2;
+
+tapTempo.cookie = ({
+	initialize: function() {
+		this.cookieData = new Mojo.Model.Cookie("netBradleyGraberTapTempo");
+		this.storeCookie();
+		storedData = this.cookieData.get();
+		if (storedData && storedData.version == "1.1.0") {
+			tapTempo.resetDuration = storedData.resetDuration;
+		}
+		this.storeCookie();
+	},
+	storeCookie: function() {
+		this.cookieData.put({  
+			version: "1.1.0",
+			resetDuration: tapTempo.resetDuration,
+		});		
+	}
+});
+
 
 function MainAssistant() {
 
 }
 
 MainAssistant.prototype.setup = function() {
-	tapTempo.elements.tempo = this.controller.get('tempo')
-	tapTempo.elements.accel = this.controller.get('accel')
+	var local = {}
+	
+	tapTempo.cookie.initialize();
+	tapTempo.elements.avgTempo = this.controller.get('avgTempo')
+	tapTempo.elements.currentTempo = this.controller.get('currentTempo')
+
+	this.controller.setupWidget('resetDuration2', {
+		label: $L(" "),
+		modelProperty: 'value',
+		min: 1,
+		max: 5,
+	}, model = {
+		value: tapTempo.resetDuration
+	});
+
+	this.controller.setupWidget('resetDuration', {
+		label: $L(" "),
+		modelProperty: 'value',
+		min: 1,
+		max: 5,
+	}, model = {
+		value: tapTempo.resetDuration
+	});
+
+
+	this.controller.listen(this.controller.get('resetDuration'),Mojo.Event.propertyChange, this.resetDuration.bindAsEventListener(this));
+	this.controller.listen(this.controller.get('resetDuration2'),Mojo.Event.propertyChange, this.resetDuration.bindAsEventListener(this));
 	this.controller.listen(this.controller.sceneElement, Mojo.Event.keypress, this.keyPressed.bindAsEventListener(this))
 	this.controller.listen(this.controller.document, Mojo.Event.tap, this.keyPressed.bindAsEventListener(this))
+}
+
+MainAssistant.prototype.reset = function () {
+	tapTempo.count = 0;
+	tapTempo.duration = 0;
+	tapTempo.prevDuration = 0;
+	tapTempo.initialBPM = 0;
+}
+MainAssistant.prototype.resetDuration = function (event) {
+	tapTempo.resetDuration = event.value;
+	tapTempo.cookie.storeCookie();
 }
 
 MainAssistant.prototype.keyPressed = function (event) {
 	var local = {}
 	local.time = new Date().getTime()
 	
-	currentDuration = local.time - tapTempo.time;
-	if (tapTempo.count != 0 && currentDuration < 2000)
-		tapTempo.duration += currentDuration;
-	else if (currentDuration > 2000 ) {
-		tapTempo.count = 0;
-		tapTempo.duration = 0;
-	}
+	local.currentDuration = local.time - tapTempo.time;
+
+	if (tapTempo.count != 0 && local.currentDuration < (tapTempo.resetDuration * 1000))
+		tapTempo.duration += local.currentDuration;
+	else if (local.currentDuration > (tapTempo.resetDuration * 1000) )
+		this.reset();
+
 	tapTempo.count++;
-	tapTempo.time = new Date().getTime();
 	
-	if (tapTempo.duration == 0)
-		tapTempo.elements.tempo.update("First Beat")
-	else
-		tapTempo.elements.tempo.update((((tapTempo.count - 1) / (tapTempo.duration / 1000)) * 60).toFixed(1) + " bpm")
+	local.avgBPM = (((tapTempo.count - 1) / (tapTempo.duration / 1000)) * 60).toFixed(1)
+	local.currentBPM = ((2 / ((local.currentDuration + tapTempo.prevDuration) / 1000)) * 60).toFixed(1)
+	
+	if (tapTempo.duration == 0) 
+		tapTempo.elements.avgTempo.update("First Beat")
+	else {
+		tapTempo.elements.avgTempo.update(local.avgBPM + " bpm")
+		tapTempo.elements.currentTempo.update(local.currentBPM + " bpm")
+	}
+
+	tapTempo.time = new Date().getTime();
+	tapTempo.prevDuration = local.currentDuration;
 }
 
 MainAssistant.prototype.activate = function(event) {
@@ -46,9 +110,13 @@ MainAssistant.prototype.deactivate = function(event) {
 }
 
 MainAssistant.prototype.cleanup = function(event) {
+	this.controller.stopListening(this.controller.get('resetDuration'),Mojo.Event.propertyChange, this.resetDuration.bindAsEventListener(this));
+	this.controller.stopListening(this.controller.get('resetDuration2'),Mojo.Event.propertyChange, this.resetDuration.bindAsEventListener(this));
 	this.controller.stopListening(this.controller.sceneElement, Mojo.Event.keypress, this.keyPressed.bindAsEventListener(this))
 	this.controller.stopListening(this.controller.document, Mojo.Event.tap, this.keyPressed.bindAsEventListener(this))
+	tapTempo.cookie.storeCookie();
 }
+
 
 /*
  * Handles the application pulldown menu
@@ -59,10 +127,18 @@ MainAssistant.prototype.handleCommand = function (event) {
 	{	event.stopPropagation(); 
 	}
 
+	if (event.type == Mojo.Event.commandEnable &&
+	   (event.command == Mojo.Menu.prefsCmd)) 
+	{	event.stopPropagation(); 
+	}
+
 	if (event.type == Mojo.Event.command) {
 		switch (event.command) {
 			case Mojo.Menu.helpCmd:
 				Mojo.Controller.stageController.pushAppSupportInfoScene();
+				break;
+			case Mojo.Menu.prefsCmd:
+				Mojo.Controller.stageController.pushScene('preferences');
 				break;
 		}
 	}
