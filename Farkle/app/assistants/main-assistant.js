700fd38f003a0138d82b50baa548806cc54b5e83
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-05-10 21:54:42 +0000

    insert sort

--- Farkle/app/assistants/main-assistant.js
+++ Farkle/app/assistants/main-assistant.js
@@ -1,75 +1,50 @@
-function MainAssistant() {
-	this.elements = {}
-	this.die = []
-	this.currentPlayPosition = new position(264,14)
-	this.oldPlayPosition = new position(264,14)
-	this.playArea = new board();
-}
-
+function MainAssistant() {}
 
 MainAssistant.prototype.setup = function() {
-	Mojo.Log.info('****************Started Setup Function****************')
-	Mojo.Log.info(' ')
-	this.getElements();	
+	Mojo.Log.warn(' ');Mojo.Log.warn(' ');Mojo.Log.warn(' ');Mojo.Log.warn(' ');
+	Mojo.Log.warn('****************Started Setup Function****************')
 
-	this.stopShaking = Mojo.Function.debounce(undefined, this.doStopShaking.bind(this), 1);
-	this.controller.listen(this.elements.cup, Mojo.Event.tap, this.shakeCup.bindAsEventListener(this))
-	this.controller.listen(this.die[1].getHandler(), Mojo.Event.tap, this.die1Tapped.bindAsEventListener(this))
-	this.controller.listen(this.die[2].getHandler(), Mojo.Event.tap, this.die2Tapped.bindAsEventListener(this))
-	this.controller.listen(this.die[3].getHandler(), Mojo.Event.tap, this.die3Tapped.bindAsEventListener(this))
-	this.controller.listen(this.die[4].getHandler(), Mojo.Event.tap, this.die4Tapped.bindAsEventListener(this))
-	this.controller.listen(this.die[5].getHandler(), Mojo.Event.tap, this.die5Tapped.bindAsEventListener(this))
-	this.controller.listen(this.die[6].getHandler(), Mojo.Event.tap, this.die6Tapped.bindAsEventListener(this))
+	this.initialize();	
+
+	this.controller.listen(this.playArea.getCupHandler(), Mojo.Event.tap, this.cupTapped.bindAsEventListener(this))
+	for (var x = 1; x <= 6; x++)
+		this.controller.listen(this.playArea.getDieHandler(x), Mojo.Event.tap, this.dieTapped.bindAsEventListener(this))
 };
 
 MainAssistant.prototype.cleanup = function(event) {
-	Mojo.Log.info('***************Started Cleanup Function***************')
-	Mojo.Log.info(' ')
-	this.controller.stopListening(this.elements.cup, Mojo.Event.tap, this.shakeCup.bindAsEventListener(this))
-	this.controller.stopListening(this.die[1].getHandler(), Mojo.Event.tap, this.die1Tapped.bindAsEventListener(this))
-	this.controller.stopListening(this.die[2].getHandler(), Mojo.Event.tap, this.die2Tapped.bindAsEventListener(this))
-	this.controller.stopListening(this.die[3].getHandler(), Mojo.Event.tap, this.die3Tapped.bindAsEventListener(this))
-	this.controller.stopListening(this.die[4].getHandler(), Mojo.Event.tap, this.die4Tapped.bindAsEventListener(this))
-	this.controller.stopListening(this.die[5].getHandler(), Mojo.Event.tap, this.die5Tapped.bindAsEventListener(this))
-	this.controller.stopListening(this.die[6].getHandler(), Mojo.Event.tap, this.die6Tapped.bindAsEventListener(this))
+	Mojo.Log.warn(' ');Mojo.Log.warn(' ');Mojo.Log.warn(' ');Mojo.Log.warn(' ');
+	Mojo.Log.warn('***************Started Cleanup Function***************')
+
+	this.controller.stopListening(this.playArea.getCupHandler(), Mojo.Event.tap, this.cupTapped.bindAsEventListener(this))
+	for (var x = 1; x <= 6; x++)
+		this.controller.stopListening(this.playArea.getDieHandler(x), Mojo.Event.tap, this.dieTapped.bindAsEventListener(this))
 };
 
-MainAssistant.prototype.shakeCup = function () {
-	this.elements.cup.addClassName('shake')
-	this.stopShaking();
-}
-MainAssistant.prototype.doStopShaking = function() {
-	this.elements.cup.removeClassName('shake')
-	for (var x = 1; x <= 6; x++) {
-		this.die[x].show();
-		this.die[x].setPosition(this.die[x].getBoardPos())
-	}
+MainAssistant.prototype.cupTapped = function () { 
+	this.playArea.roll()
 }
 
-MainAssistant.prototype.dieTapped = function(die) {
-	this.playArea.dieTapped(this.die[die])
-/*
-	this.oldPlayPosition.set(this.currentPlayPosition)
-	this.currentPlayPosition.set(this.die[die].tapped(this.currentPlayPosition))
-	if (this.oldPlayPosition.getLeft() > this.currentPlayPosition.getLeft())
-		for (var x = 1; x <= 6; x++)
-			this.die[x].moveLeft()
-*/
+MainAssistant.prototype.dieTapped = function(event) {
+	this.playArea.dieTapped(parseInt(event.srcElement.id.substring(3)))
 }
 
-MainAssistant.prototype.getElements = function () {
-	this.elements.cup = this.controller.get('cup');
-
+MainAssistant.prototype.initialize = function () {
+	var dice = []
 	for (var x = 1; x <= 6; x++)
-		this.die[x] = new die(x, this.controller.get('die' + x))
+		dice[x] = new die(x, this.controller.get('die' + x))
+
+	this.playArea = new board(this.controller.get('cup'), dice);
 }
 
 MainAssistant.prototype.activate = function(event) {};
 MainAssistant.prototype.deactivate = function(event) {};
 
-MainAssistant.prototype.die1Tapped = function(event){ Mojo.Log.info('value =' + Object.toJSON(event.srcElement)); this.dieTapped(1) }
-MainAssistant.prototype.die2Tapped = function(){ this.dieTapped(2) }
-MainAssistant.prototype.die3Tapped = function(){ this.dieTapped(3) }
-MainAssistant.prototype.die4Tapped = function(){ this.dieTapped(4) }
-MainAssistant.prototype.die5Tapped = function(){ this.dieTapped(5) }
-MainAssistant.prototype.die6Tapped = function(){ this.dieTapped(6) }
+/*
+MainAssistant.prototype.die1Tapped = function(event){
+	for (var prop in event.srcElement) {
+		Mojo.Log.info("PROPERTY: " + prop);
+		Mojo.Log.info("==> " + event.srcElement[prop]);
+	}
+	this.dieTapped(1)
+}
+*/
--- Farkle/app/class-definitions/board-class.js
+++ Farkle/app/class-definitions/board-class.js
@@ -1,40 +1,96 @@
-function board() {
+function board(cupHandler, dice) {
+	this.state = 'unrolled'
+	this.cupHandler = cupHandler
+	this.die = dice
 	this.layer = 1;
-	this.die = []
+	this.playGrid = []
 	this.positionGrid = []
 	for (var x = 1; x <=6; x++)
 		this.positionGrid[x] = []
 		
 	for (var x = 0; x <= 5; x++) 
-		for (var y = 0; y <= 5; y++) {
+		for (var y = 0; y <= 5; y++)
 			this.positionGrid[x + 1][y + 1] = new position(264 - (x * 50), (y * 50) + 14)
-		}
 }
 
-board.prototype.dieTapped = function(die) {
+board.prototype.getCupHandler = function () { return this.cupHandler }
+board.prototype.getDieHandler = function (die) { return this.die[die].getHandler() }
+
+board.prototype.roll = function () {
+	if (this.state == 'unrolled') {
+		this.cupHandler.addClassName('shake')
+		this.stopShaking = Mojo.Function.debounce(undefined, this.doStopShaking.bind(this), 1);
+		this.stopShaking();
+		this.state = 'rolled'
+		this.cupHandler.src = 'images/FeltCup.png'
+	}
+	else if (this.state == 'rolled') {
+		
+	}
+}
+
+board.prototype.doStopShaking = function() {
+	this.cupHandler.removeClassName('shake')
+	for (var x = 1; x <= 6; x++) {
+		this.die[x].roll();
+		this.die[x].show();
+		this.die[x].setPosition(this.die[x].getBoardPos(), true)
+	}
+}
+
+board.prototype.dieTapped = function(dieNum) {
 	var move = false
-	if (die.isNotInPlay()) {
+	var bump = false
+	var storedDie = undefined
+	var tmpDie = undefined
+	if (this.die[dieNum].isNotInPlay()) {
 		for (var x = 1; x <= 6; x++) 
-			if (!this.die[x]) {
-				this.die[x] = die
-				this.die[x].setPosition(this.positionGrid[this.layer][x])
+			if (!this.playGrid[x] && !bump) {
+				Mojo.Log.warn('no bump insert')
+				this.playGrid[x] = this.die[dieNum]
+				this.playGrid[x].setPosition(this.positionGrid[this.layer][x], false)
 				break;
+			}
+			else if (!this.playGrid[x] && bump) {
+				Mojo.Log.warn('bump insert')
+				this.playGrid[x] = storedDie
+				this.playGrid[x].setPosition(this.positionGrid[this.layer][x], false)
+				break;
+			}
+			else if (this.die[dieNum].getValue() < this.playGrid[x].getValue()) {
+				Mojo.Log.warn('initial bump - x=' + x)
+				storedDie = this.playGrid[x]
+				this.playGrid[x] = this.die[dieNum]
+				this.playGrid[x].setPosition(this.positionGrid[this.layer][x], false)
+				bump = true;
+			}
+			else if (this.playGrid[x] && bump) {
+				Mojo.Log.warn(x + ' bump')
+				tmpDie = this.playGrid[x]
+				this.playGrid[x] = storedDie
+				this.playGrid[x].setPosition(this.positionGrid[this.layer][x], false)
+				storedDie = tmpDie
 			}
 	}
 	else {
 		for (var x = 1; x <= 6; x++) {
-			if (this.die[x] && move) {
-				this.die[x].moveLeft();
-				this.die[x-1] = this.die[x]
-				this.die[x] = undefined
+			if (this.playGrid[x] && move) {
+				this.playGrid[x].moveLeft();
+				this.playGrid[x-1] = this.playGrid[x]
+				this.playGrid[x] = undefined
 			}
-			else if (this.die[x] && this.die[x].getId() == die.getId()) {
-				die.setPosition(die.getBoardPos())
-				this.die[x] = undefined
+			else if (this.playGrid[x] && this.playGrid[x].getId() == this.die[dieNum].getId()) {
+				this.die[dieNum].setPosition(this.die[dieNum].getBoardPos(), true)
+				this.playGrid[x] = undefined
 				move = true;
 			}
-			else if (!this.die[x]) 
+			else if (!this.playGrid[x]) 
 				move = true;
 		}
 	}
+	this.checkPlayGrid()
+}
+
+board.prototype.checkPlayGrid = function () {
+	
 }
