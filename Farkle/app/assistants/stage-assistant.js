ef6d6a2c1216a82641e305a94b2d96434d8e2631
bradgraber <bradgraber@3ca0e812-0296-11de-99c0-75d572ba1ddd>
2010-05-20 17:52:23 +0000

    locked the volume buttons

--- Farkle/app/assistants/stage-assistant.js
+++ Farkle/app/assistants/stage-assistant.js
@@ -2,9 +2,55 @@ function StageAssistant() {
 }
 
 StageAssistant.prototype.setup = function() {
+//	this.controller.listen(Mojo.Event.deactivate, this.onBlurHandler.bind(this));
+//	this.controller.listen(Mojo.Event.activate, this.onFocusHandler.bind(this));
+
+	window.document.addEventListener (Mojo.Event.deactivate, this.onBlurHandler.bind(this));
+	window.document.addEventListener (Mojo.Event.activate, this.onFocusHandler.bind(this));
+
+	this.foregroundVolumeMarker = this.markAppForeground();
+
 	this.controller.pushScene({
 		name: "main",
 		transition: Mojo.Transition.crossFade,
 		disableSceneScroller: true
 	});
 };
+
+StageAssistant.prototype.onBlurHandler = function(){
+	this.lostFocus = true;
+
+	if (this.foregroundVolumeMarker){
+		this.foregroundVolumeMarker.cancel();
+		this.foregroundVolumeMarker = null;
+	}
+		
+}
+
+StageAssistant.prototype.onFocusHandler = function(){
+	this.lostFocus = false; 
+
+	if (this.pendingRefresh){
+		this.forceRefreshActiveScene();
+		this.pendingRefresh = false;
+	}
+	
+	if (!this.foregroundVolumeMarker)
+		this.foregroundVolumeMarker = this.markAppForeground();
+
+	if (this.musicPlayer && this.musicPlayer.isSuspended()){
+		this.musicPlayer.unsuspend();
+	}
+}
+
+StageAssistant.prototype.markAppForeground = function(callback){
+	var parameters = {};
+	parameters.subscribe = true;
+	parameters.foregroundApp = true;
+
+	return new Mojo.Service.Request("palm://com.palm.audio/media", {
+		method: 'lockVolumeKeys',
+		onSuccess: callback,
+		parameters: parameters
+	});
+}
